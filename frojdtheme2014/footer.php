379d2b427fb53dc439e0ee1e37e2065884f8451c
saraojelind <sara.ojelind@frojd.se>
2015-02-02 09:35:27 +0100

    Added Fröjd Theme 2014

---
+++ frojdtheme2014/footer.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains footer content and the closing of the #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage Frojd_Theme_2014
+ * @since Fröjd Theme 2014 1.0
+ */
+?>
+
+		</div><!-- #main -->
+		<footer id="colophon" class="site-footer" role="contentinfo">
+			<div class="site-info">
+				<a href="<?php echo esc_url( __( 'http://wordpress.org/', get_translation_domain() ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', get_translation_domain() ); ?>"><?php printf( __( 'Proudly powered by %s', get_translation_domain() ), 'WordPress' ); ?></a>
+			</div><!-- .site-info -->
+		</footer><!-- #colophon -->
+	</div><!-- #page -->
+
+	<?php wp_footer(); ?>
+</body>
+</html>
